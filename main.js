2139755060f59e792f893337dbeaaea35e439eb0
ttiop2289 <127741157+ttiop2289@users.noreply.github.com>
2023-06-28 12:05:42 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function checkAnswer(answer) {
+    var correctAnswer = "Блакитного";
+  
+    if (answer === correctAnswer) {
+      alert("Відповідь правильна!");
+    } else {
+      alert("Відповідь неправильна. Спробуйте ще раз.");
+    }
+  }
